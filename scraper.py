88027fda9e246f8889ba38a7e006510647e47a9c
Jack Qiu <jack.qiu@frogdesign.com>
2016-11-18 13:52:03 +0800

    remove the generic error detection

--- scraper.py
+++ scraper.py
@@ -1,18 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError, URLError
+#from IOError import IOError
 #先判断是否能获取页面
 def scraper():
     try:
-        html = urlopen('http://nn.focus.cn/xxsearch/index.html');
+        html = urlopen('http://nn.XXXfocus.cn/xxsearch/index.html');
     #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
-    except HTTPError as e:
-        print('can not find web page')
+    except IOError as e:
+        print('can not reach the page【/(ㄒoㄒ)/~~】')
         print(e)
     
-    #通用的错误检查
-    except:
-        print(e)
     else: 
         bsObj = BeautifulSoup(html.read())
         print(bsObj.h1)
