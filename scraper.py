af6705dd2d17babfeb279034d717943dcdfb6750
Jack Qiu <jack.qiu@frogdesign.com>
2016-11-18 15:00:07 +0800

    use set() to store pages

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError, URLError
+
 #你自己的配置文件，请将config-sample.py重命名为config.py,然后填写对应的值即可
 import config
-#from IOError import IOError
-#先判断是否能获取页面
 def scraper():
+    #定义链接集合，以免链接重复
+    pages = set()
+
+    #先判断是否能获取页面
     try:
         html = urlopen(config.value['url']);
     #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
@@ -22,6 +25,7 @@ def scraper():
         #我倾向只获取详情页的URL，然后在详情页中获取更多的信息
         for houseItem in houseItems:
             houseUrl = houseItem.find('a', {'class','_click'})['href']
-            print(houseUrl)
+            pages.add(houseUrl)
+        print(pages)
 
 if __name__ == '__main__': scraper()
