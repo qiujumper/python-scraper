fc58e5a98eb8a1c16b5023ee1c4771bea666541c
Jack Qiu <jack.qiu@frogdesign.com>
2016-11-18 15:22:20 +0800

    use requests to simulate  real people to view browser

--- scraper.py
+++ scraper.py
@@ -1,23 +1,34 @@
+#获取页面对象
 from urllib.request import urlopen
+from urllib.request import urlretrieve
+#修改请求头模块,模拟真人访问
+import requests
+#选择器
 from bs4 import BeautifulSoup
-from urllib.error import HTTPError, URLError
+#正则对象
+import re
+#获取随机数
+import random
+#获取日期
+import datetime
 
 #你自己的配置文件，请将config-sample.py重命名为config.py,然后填写对应的值即可
 import config
 def scraper():
     #定义链接集合，以免链接重复
     pages = set()
+    session = requests.Session()
 
     #先判断是否能获取页面
     try:
-        html = urlopen(config.value['url']);
+        req = session.get(config.value['url'], headers = config.value['headers'])
     #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
     except IOError as e:
         print('can not reach the page【/(ㄒoㄒ)/~~】')
         print(e)
     
     else: 
-        bsObj = BeautifulSoup(html.read())
+        bsObj = BeautifulSoup(req.text)
         #获取第一页的所有房子模块
         houseItems = bsObj.select('.s-lp-list .lp-list-li')
 
@@ -27,5 +38,16 @@ def scraper():
             houseUrl = houseItem.find('a', {'class','_click'})['href']
             pages.add(houseUrl)
         print(pages)
+
+    def getItemDetails(url):
+        #先判断是否能获取页面
+        try:
+            html = urlopen(url);
+        #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
+        except IOError as e:
+            print('can not reach the page【/(ㄒoㄒ)/~~】')
+            print(e)
+
+
 
 if __name__ == '__main__': scraper()
