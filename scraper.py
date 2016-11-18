b97c95db72a5d6ef3e17738fab28421a03173293
Jack Qiu <jack.qiu@frogdesign.com>
2016-11-18 14:20:15 +0800

    get url for page one successfullly

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from urllib.error import HTTPError, URLError
 #先判断是否能获取页面
 def scraper():
     try:
-        html = urlopen('http://nn.XXXfocus.cn/xxsearch/index.html');
+        html = urlopen('http://nn.focus.cn/search/index.html');
     #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
     except IOError as e:
         print('can not reach the page【/(ㄒoㄒ)/~~】')
@@ -13,6 +13,13 @@ def scraper():
     
     else: 
         bsObj = BeautifulSoup(html.read())
-        print(bsObj.h1)
+        #获取第一页的所有房子模块
+        houseItems = bsObj.select('.s-lp-list .lp-list-li')
+
+        #从模块中提取我们需要的信息，比如详情页的URL,价格，略缩图等
+        #我倾向只获取详情页的URL，然后在详情页中获取更多的信息
+        for houseItem in houseItems:
+            houseUrl = houseItem.find('a', {'class','_click'})['href']
+            print(houseUrl)
 
 if __name__ == '__main__': scraper()
