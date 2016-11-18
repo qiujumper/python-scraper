3ffbe11e9f0a0206b39487bf1de291ed1a84ebbb
Jack Qiu <jack.qiu@frogdesign.com>
2016-11-18 14:37:23 +0800

    add .gitignore file

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError, URLError
+#你自己的配置文件，请将config-sample.py重命名为config.py,然后填写对应的值即可
+import config
 #from IOError import IOError
 #先判断是否能获取页面
 def scraper():
     try:
-        html = urlopen('http://nn.focus.cn/search/index.html');
+        html = urlopen(config.value['url']);
     #这个判断只能判定是不是404或者500的错误，如果DNS没法解析，是无法判定的
     except IOError as e:
         print('can not reach the page【/(ㄒoㄒ)/~~】')
